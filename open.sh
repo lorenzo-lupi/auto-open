b592f2080840cb1c735f234b682c27e94ab6d6b6
lorenzo-lupi <lupi.lorenzo001@gmail.com>
2024-07-05 16:31:47 +0200

    Fixed closing related issues and booleans

--- open.sh
+++ open.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+false=0
+true=1
 
 read_default() {
-	while IFS'=' read -r ext program; do
+
+	while IFS='=' read -r ext program; do
 		[[ "$ext" =~ ^#.* ]] || [[ -z "$ext" ]] && continue
 
 		if [[ "$ext" == "DEFAULT" ]]; then
+			found=$true
 			default_reader="$program"
 		fi
 	done < "$CONF_FILE"
 
-	echo "NO DEFAULT PROGRAM FOUND" 1>&2
-	exit 1
+	if [ $found -eq $false ]; then
+		echo "NO DEFAULT PROGRAM FOUND" 1>&2
+		exit 1
+	fi
 }
 
 read_file_extension() {
@@ -18,17 +24,22 @@ read_file_extension() {
 }
 
 exec_program() {
+	found=$false
 	while IFS='=' read -r ext program; do
 	
 		[[ "$ext" =~ ^#.* ]] || [[ -z "$ext" ]] && continue
 
 		if [ "$ext" = "$file_extension" ]; then
-			exec "$program" "$1"
+			found=$true
+			"$program" "$1" 
 			exit 0
 		fi
 	done < "$CONF_FILE"
 	
-	exec "$default_reader" "$1"
+	if [ $found -eq $false ]; then
+		"$default_reader" "$1"
+		exit 0
+	fi
 }
 
 if [ $# -lt 1 ]; then
@@ -36,7 +47,8 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-CONF_FILE="programs.cfg"
+CONF_FILE="${AUTO_OPEN_CONF}programs.cfg"
 
+read_default 
 read_file_extension "$1"
 exec_program "$1"
